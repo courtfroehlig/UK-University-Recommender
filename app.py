a3497df15d7511984e843e9e9ab0e8a3ce1fec58
MarioCekic <makacov@gmail.com>
2021-03-11 22:01:36 +0100

    tweak

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-sfrom flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template
 import pickle
+import pandas as pd
 import numpy as np
+import sys
 
 app = Flask(__name__)
 
-#use pickle to load pre-trained model
-model = pickle.load(open('uni_rec.pkl', 'rb'))
+# use pickle to load pre-trained model
+# model = pickle.load(open('uni_rec.pkl', 'rb'))
+model = pickle.load(open('models/uni_wizz_v0.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -13,13 +16,15 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    int_features = [float(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-    
-    
-    return render_template('index.html', prediction_text='You should apply to {}'.format(prediction))
+    raw_features = request.form.to_dict()
+    final_features = pd.DataFrame(
+        [raw_features.values()],
+        columns=raw_features.keys())
+
+    prediction = model.predict(final_features)[0]
+
+    return render_template('index.html',\
+        prediction_text='You should apply to ' + str(prediction) + '.')
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
